c196171b1b9ed2cd2771f3edd158427e7850b2f1
SHOUTSE <frankchang1983@gmail.com>
2015-12-12 14:28:56 +0800

    change resourse to events from welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :people
   get "welcome/say_hello" => "welcome#say"
   get "welcome" => "welcome#index"
-  root :to => "welcome#index"
+  root :to => "events#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
