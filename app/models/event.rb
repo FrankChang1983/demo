0866c562bdbe28ce48f993052dbd9ddd944f7d2b
SHOUTSE <frankchang1983@gmail.com>
2016-01-02 22:38:18 +0800

    設定照片插件

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,7 @@ class Event < ActiveRecord::Base
   has_one :location
   accepts_nested_attributes_for :location, :allow_destroy => true, :reject_if => :all_blank
 
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+
 end
